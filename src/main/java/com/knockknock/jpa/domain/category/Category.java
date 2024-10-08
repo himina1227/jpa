012b2e07be76341d008e98da94e24edb36da3c97
nakgyeom <himina1227@beplushealthcare.com>
2024-10-08 18:41:48 +0900

    - Category 추가

---
+++ src/main/java/com/knockknock/jpa/domain/category/Category.java
@@ -0,0 +1,32 @@
+package com.knockknock.jpa.domain.category;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "parent")
+    private Set<Category> children = new HashSet<>();
+}
